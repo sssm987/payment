aff504de16aa0ecdb674d8344524e51cde6abbb9
DESKTOP-P254RB0\sssm9 <sssm9@naver.com>
2026-08-05 10:10:19 +0900

    feat: payment main 추가

--- src/main/java/org/example/payment/api/order/dto/request/OrderCreateRequestDTO.java
+++ src/main/java/org/example/payment/api/order/dto/request/OrderCreateRequestDTO.java
@@ -1,7 +1,11 @@
 package org.example.payment.api.order.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record OrderCreateRequestDTO(
+        @Schema(description = "상품 번호", example = "5")
         long productId,
+        @Schema(description = "회원 번호", example = "10")
         long memberId
 ){
 }
